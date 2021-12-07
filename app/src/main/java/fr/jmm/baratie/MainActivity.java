44e3ba9990402091439aecc9adcd986dad6bfd9c
wayry <wayrygaming@gmail.com>
2021-12-07 09:55:32 +0100

    ui home

--- app/src/main/java/fr/jmm/baratie/MainActivity.java
+++ app/src/main/java/fr/jmm/baratie/MainActivity.java
@@ -2,7 +2,6 @@ package fr.jmm.baratie;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,7 +11,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -102,6 +100,24 @@ public class MainActivity extends AppCompatActivity {
         ingredientPizza.put(sauceTomate, (double) 3);
         ingredientPizza.put(mozzarella, (double) 125);
         ingredientPizza.put(jambon, (double) 2);
+        HashMap<Ingredient, Double> ingredientCookie = new HashMap<>();
+        Ingredient oeuf = new Ingredient("Oeuf","nombre");
+        Ingredient beurreDoux = new Ingredient("Beurre Doux","grammes");
+        Ingredient pepiteChoco = new Ingredient("Pépite de Chocolat","grammes");
+        Ingredient levureChimique = new Ingredient("Levure Chimique","cuillère(s) à café");
+        Ingredient sucre = new Ingredient("Sucre", "grammes");
+        Ingredient sucreVanille = new Ingredient("Sucre vanillé", "sachet");
+        Ingredient sel = new Ingredient("Sel", "cuillère(s) à café");
+        ingredientCookie.put(oeuf, (double) 1);
+        ingredientCookie.put(beurreDoux,(double) 85);
+        ingredientCookie.put(pepiteChoco,(double) 100);
+        ingredientCookie.put(levureChimique,(double) 1);
+        ingredientCookie.put(sucre,(double) 85);
+        ingredientCookie.put(farine,(double) 150);
+        ingredientCookie.put(sucreVanille,(double) 1);
+        ingredientCookie.put(sel, 0.5);
+
+
 
         ingredients.add(farine);
         ingredients.add(eau);
@@ -112,9 +128,45 @@ public class MainActivity extends AppCompatActivity {
 
         Recette pizza = new Recette("Pizza", 6, 45,
                 "Une pizza tah délicieuse", ingredientPizza);
+        Recette cookie = new Recette("Cookie", 6, 25,
+                "Des cookies chococroco", ingredientCookie);
+
+        Recette feuilletesSaucisse = new Recette("Feuilletés apéritif à la saucisse", 6, 20,
+                "Un apéro tah j'ai faim", ingredientCookie);
+        HashMap<Ingredient, Double> ingredientfeuilletesSaucisse = new HashMap<>();
+
+        Ingredient pateFeuilletee = new Ingredient("Pâte feuilletée", "nombre");
+        ingredientfeuilletesSaucisse.put(pateFeuilletee, (double) 1);
+        Ingredient gruyereRape = new Ingredient("Gruyère rapé", "grammes");
+        ingredientfeuilletesSaucisse.put(gruyereRape, (double) 75);
+        Ingredient saucisse = new Ingredient("Saucisse", "nombre");
+        ingredientfeuilletesSaucisse.put(saucisse, (double) 3);
+
+        Recette tartiflette = new Recette("La vraie Tartiflette", 4, 75,
+                "Une tartiflette tah lewandoski qui te fait peter le bide", ingredientCookie);
+        HashMap<Ingredient, Double> ingredientTartiflette = new HashMap<>();
+
+        Ingredient pommeDeTerre = new Ingredient("de Pomme de terre", "kilogrammes");
+        ingredientTartiflette.put(pommeDeTerre, (double) 1);
+        Ingredient huile = new Ingredient("d'Huile", "cuillère(s) à soupe");
+        ingredientTartiflette.put(huile, (double) 2);
+        Ingredient ail = new Ingredient("d'Ail", "gousses");
+        ingredientTartiflette.put(ail, (double) 1);
+        ingredientTartiflette.put(sel, (double) 3);
+        Ingredient poivre = new Ingredient("Poivre", "cuillère(s) à café");
+        ingredientTartiflette.put(poivre, (double) 3);
+        Ingredient lardonFume = new Ingredient("Lardons Fumés", "grammes");
+        ingredientTartiflette.put(lardonFume, (double) 200);
+        Ingredient oignon = new Ingredient("Oignons", "grammes");
+        ingredientTartiflette.put(oignon, (double) 200);
+        Ingredient roblochon = new Ingredient("Roblochon", "nombre");
+        ingredientTartiflette.put(roblochon, (double) 1);
 
         recettes.add(pizza);
         recettes.add(patesCarbo);
+        recettes.add(cookie);
+        recettes.add(feuilletesSaucisse);
+        recettes.add(tartiflette);
 
 
     }
