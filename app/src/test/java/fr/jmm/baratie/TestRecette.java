35602459619410cb2c7666ee429a8a06648e84ea
Maxime <maximeecd@gmail.com>
2021-12-13 18:20:08 +0100

    change ingredient

---
+++ app/src/test/java/fr/jmm/baratie/TestRecette.java
@@ -0,0 +1,28 @@
+package fr.jmm.baratie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import fr.jmm.baratie.metier.Recette;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class TestRecette {
+
+    @Test
+    public void testRecette() {
+        Recette maRecette = new Recette("Hamburger", 2, 30,
+                "Un Hamburger succulant sur son lit de salade", "4 pains burger, deux steak hachés, 4 tranches de cheddar, sauce ketchup, salade 200g");
+
+        assertTrue(maRecette.getIntitule() == "Hamburger");
+
+        assertTrue(maRecette.getNbPers(2));
+
+
+        ;
+    }
+}
